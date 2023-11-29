10cefc49ba53e81562d2039b985fc51e25ad29bc
GLEB <gleb.gavrilov.17@bk.ru>
2023-11-29 09:26:58 +0300

    ui + treug

--- Game.py
+++ Game.py
@@ -1,7 +1,7 @@
 import sys
-
-from PyQt5 import uic
-from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QMainWindow
+import time
+from PyQt5 import uic, QtCore
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QMainWindow, QLabel
 
 
 class Lobby(QMainWindow):
@@ -23,10 +23,36 @@ class Game(QWidget):
     def __init__(self):
         super(Game, self).__init__()
         self.initUI()
+        self.gameButton00.hide()
+        self.Time.hide()
 
     def initUI(self):
-        self.setWindowTitle('Game')
-        self.setGeometry(300, 300, 300, 300)
+        uic.loadUi('Game.ui', self)
+        self.StartBtn.clicked.connect(self.StartGame)
+        self.otschot = QLabel(self)
+
+    def StartGame(self):
+        self.StartBtn.hide()
+        self.otschot.move(200, 200)
+        self.otschot.resize(200, 20)
+        self.game()
+
+    def game(self):
+        self.gaming = True
+        timer = QtCore.QTimer()
+        timer.timeout.connect(self.endGame)
+        timer.start(1000)
+        #while self.gaming:
+            #lvl = 1
+            #for i in range(lvl + 2):
+                #for j in range(lvl + 2):
+                    #pass
+
+
+    def endGame(self):
+        return None
+
+
 
 
 if __name__ == '__main__':
