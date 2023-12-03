cfd89034ba334a676283dafdf9c91638bd52ec25
GLEB <gleb.gavrilov.17@bk.ru>
2023-12-03 21:50:33 +0300

    game is complete

--- Game.py
+++ Game.py
@@ -27,10 +27,13 @@ class Game(QWidget):
     def __init__(self, coun):
         super(Game, self).__init__()
         self.initUI()
+        self.color_True = (random.choice([255, 0]), 255, random.choice([255, 0]))
         self.hide_buttons_and_connect()
+        self.setButtonColorBase()
         self.Gameover_text.hide()
         self.timeBar.setValue(100)
         self.count = coun
+
 
     def hide_buttons_and_connect(self):
         self.gameButton_00.hide()
@@ -83,6 +86,111 @@ class Game(QWidget):
         self.gameButton_41.clicked.connect(self.FalseButton)
         self.gameButton_42.clicked.connect(self.FalseButton)
 
+    def setButtonColorBase(self):
+        # Устанавливаем цвет кнопок
+        self.gameButton_00.setStyleSheet('background-color: #FFFFFF')
+        self.gameButton_01.setStyleSheet('background-color: #FFFFFF')
+        self.gameButton_02.setStyleSheet('background-color: #FFFFFF')
+        self.gameButton_03.setStyleSheet('background-color: #FFFFFF')
+
+        self.gameButton_10.setStyleSheet('background-color: #FFFFFF')
+        self.gameButton_11.setStyleSheet('background-color: #FFFFFF')
+        self.gameButton_12.setStyleSheet('background-color: #FFFFFF')
+        self.gameButton_13.setStyleSheet('background-color: #FFFFFF')
+
+        self.gameButton_20.setStyleSheet('background-color: #FFFFFF')
+        self.gameButton_21.setStyleSheet('background-color: #FFFFFF')
+        self.gameButton_22.setStyleSheet('background-color: #FFFFFF')
+        self.gameButton_23.setStyleSheet('background-color: #FFFFFF')
+
+        self.gameButton_30.setStyleSheet('background-color: #FFFFFF')
+        self.gameButton_31.setStyleSheet('background-color: #FFFFFF')
+        self.gameButton_32.setStyleSheet('background-color: #FFFFFF')
+        self.gameButton_33.setStyleSheet('background-color: #FFFFFF')
+
+        self.gameButton_40.setStyleSheet('background-color: #FFFFFF')
+        self.gameButton_41.setStyleSheet('background-color: #FFFFFF')
+        self.gameButton_42.setStyleSheet('background-color: #FFFFFF')
+        self.gameButton_True.setStyleSheet(f'background-color: rgb{self.color_True}')
+
+    def setButtonColor(self):
+        if self.lvl > 0:
+            b = self.randomColor()
+            self.gameButton_00.setStyleSheet(f'background-color: rgb{b}')
+            self.randomFalseText('self.gameButton_00', b)
+        if self.lvl > 1:
+            b = self.randomColor()
+            self.gameButton_01.setStyleSheet(f'background-color: rgb{b}')
+            self.randomFalseText('self.gameButton_01', b)
+        if self.lvl > 2:
+            b = self.randomColor()
+            self.gameButton_02.setStyleSheet(f'background-color: rgb{b}')
+            self.randomFalseText('self.gameButton_02', b)
+        if self.lvl > 3:
+            b = self.randomColor()
+            self.gameButton_03.setStyleSheet(f'background-color: rgb{b}')
+            self.randomFalseText('self.gameButton_03', b)
+        if self.lvl > 4:
+            b = self.randomColor()
+            self.gameButton_10.setStyleSheet(f'background-color: rgb{b}')
+            self.randomFalseText('self.gameButton_10', b)
+        if self.lvl > 5:
+            b = self.randomColor()
+            self.gameButton_11.setStyleSheet(f'background-color: rgb{b}')
+            self.randomFalseText('self.gameButton_11', b)
+        if self.lvl > 6:
+            b = self.randomColor()
+            self.gameButton_12.setStyleSheet(f'background-color: rgb{b}')
+            self.randomFalseText('self.gameButton_12', b)
+        if self.lvl > 7:
+            b = self.randomColor()
+            self.gameButton_13.setStyleSheet(f'background-color: rgb{b}')
+            self.randomFalseText('self.gameButton_13', b)
+        if self.lvl > 8:
+            b = self.randomColor()
+            self.gameButton_20.setStyleSheet(f'background-color: rgb{b}')
+            self.randomFalseText('self.gameButton_20', b)
+        if self.lvl > 9:
+            b = self.randomColor()
+            self.gameButton_21.setStyleSheet(f'background-color: rgb{b}')
+            self.randomFalseText('self.gameButton_21', b)
+        if self.lvl > 10:
+            b = self.randomColor()
+            self.gameButton_22.setStyleSheet(f'background-color: rgb{b}')
+            self.randomFalseText('self.gameButton_22', b)
+        if self.lvl > 11:
+            b = self.randomColor()
+            self.gameButton_23.setStyleSheet(f'background-color: rgb{b}')
+            self.randomFalseText('self.gameButton_23', b)
+        if self.lvl > 12:
+            b = self.randomColor()
+            self.gameButton_30.setStyleSheet(f'background-color: rgb{b}')
+            self.randomFalseText('self.gameButton_30', b)
+        if self.lvl > 13:
+            b = self.randomColor()
+            self.gameButton_31.setStyleSheet(f'background-color: rgb{b}')
+            self.randomFalseText('self.gameButton_31', b)
+        if self.lvl > 14:
+            b = self.randomColor()
+            self.gameButton_32.setStyleSheet(f'background-color: rgb{b}')
+            self.randomFalseText('self.gameButton_32', b)
+        if self.lvl > 15:
+            b = self.randomColor()
+            self.gameButton_33.setStyleSheet(f'background-color: rgb{b}')
+            self.randomFalseText('self.gameButton_33', b)
+        if self.lvl > 16:
+            b = self.randomColor()
+            self.gameButton_40.setStyleSheet(f'background-color: rgb{b}')
+            self.randomFalseText('self.gameButton_40', b)
+        if self.lvl > 17:
+            b = self.randomColor()
+            self.gameButton_41.setStyleSheet(f'background-color: rgb{b}')
+            self.randomFalseText('self.gameButton_41', b)
+        if self.lvl > 18:
+            b = self.randomColor()
+            self.gameButton_42.setStyleSheet(f'background-color: rgb{b}')
+            self.randomFalseText('self.gameButton_42', b)
+
     def show_buttons(self):
         self.gameButton_00.show()
         self.gameButton_01.show()
@@ -113,32 +221,89 @@ class Game(QWidget):
         self.StartBtn.resize(0, 0)
 
     def initUI(self):
+        self.lvl = 0
         uic.loadUi('Game.ui', self)
         self.StartBtn.clicked.connect(self.StartGame)
+        self.tab = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ]
+        self.button_group = QButtonGroup()
+        self.button_group.addButton(self.radioButtonTxt)
+        self.button_group.addButton(self.radioButtonRGB)
+        self.button_group.buttonClicked.connect(self.radio)
 
     def StartGame(self):
         self.gameButton_True.clicked.connect(self.TrueButton)
         self.timeText.setText(str(self.count))
+        self.radioButtonRGB.hide()
+        self.radioButtonTxt.hide()
         self.timeBar.setValue(self.count / (30 / 100))
         self.game()
         self.show_buttons()
+        self.new_lvl()
 
     def game(self):
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.Timeme)
         self.timer.start(1000)
 
+    def randomColor(self):
+        return random.choice([255, 0]), random.choice([255, 0]), random.choice([255, 0])
+
     def Timeme(self):
         if self.count > 1:
             self.count -= 1
             self.timeText.setText(str(self.count))
             self.timeBar.setValue(int(self.count / (30 / 100)))
-            print(self.count)
+            # print(self.count)
         else:
             self.timeText.setText(str(0))
             self.timeBar.setValue(int(0))
             self.endGame()
-            print(self.count - 1)
+            # print(self.count - 1)
+
+    def radio(self, button):
+        if button.text() == 'Текстовый режим':
+            self.rezim = 'txt'
+        elif button.text() == 'RGB режим':
+            self.rezim = 'rgb'
+        self.StartBtn.setEnabled(True)
+
+    def randomFalseText(self, a, col):
+        flag = True
+        while flag:
+            g = self.randomColor()
+            eval(a).setText(f'{g}')
+            if col == (0, 0, 0):
+                eval(a).setStyleSheet(f'background-color: rgb{col}; color: white;')
+            else:
+                eval(a).setStyleSheet(f'background-color: rgb{col}; color: black;')
+            if g != col:
+                flag = False
+        self.rgbInText(g, a)
+
+    def rgbInText(self, g, a):
+        if self.rezim == 'txt':
+            if g == (0, 0, 0):
+                eval(a).setText('Чёрный')
+            elif g == (255, 0, 0):
+                eval(a).setText('Красный')
+            elif g == (0, 255, 0):
+                eval(a).setText('Зелёный')
+            elif g == (0, 0, 255):
+                eval(a).setText('Синий')
+            elif g == (255, 0, 255):
+                eval(a).setText('Розовый')
+            elif g == (0, 255, 255):
+                eval(a).setText('Голубой')
+            elif g == (255, 255, 255):
+                eval(a).setText('Белый')
+            elif g == (255, 255, 0):
+                eval(a).setText('Жёлтый')
 
     def TrueButton(self):
         if self.count + 5 <= 30:
@@ -147,9 +312,47 @@ class Game(QWidget):
             self.timeText.setText(str(self.count))
         else:
             self.count = 30
-            print('Больше')
+            # print('Больше')
             self.timeBar.setValue(int(self.count / (30 / 100)))
             self.timeText.setText(str(self.count))
+        self.lvl_text.setText(str(self.lvl))
+        self.new_lvl()
+
+    def new_lvl(self):
+        self.lvl += 1
+        self.setButtonColor()
+        ok = True
+        for y in range(4):
+            for x in range(5):
+                try:
+                    # eval('self.gameButton_{}'.format(str(x) + str(y))).setText(str(x) + str(y))
+
+                    while ok:
+                        self.rx, self.ry = random.choice(range(0, 4)), random.choice(range(0, 5))
+                        if self.tab[self.ry][self.rx] != self.lvl:
+                            # print(f'x={self.rx}, y={self.ry}')
+                            self.tab[self.ry][self.rx] = self.lvl
+                            ok = False
+                    eval('self.gameButton_{}'.format(str(x) + str(y))).move(30 + self.rx * 100, 60 + self.ry * 100)
+                    ok = True
+                # Для True_Button
+                except Exception:
+                    while ok:
+                        self.rx, self.ry = random.choice(range(0, 4)), random.choice(range(0, 5))
+                        if self.tab[self.ry][self.rx] != self.lvl:
+                            self.tab[self.ry][self.rx] = self.lvl
+                            ok = False
+                    self.gameButton_True.move(30 + self.rx * 100, 60 + self.ry * 100)
+                    self.color_True = (random.choice([255, 0]), random.choice([255, 0]), random.choice([255, 0]))
+                    self.gameButton_True.setText(f'{self.color_True}')
+                    if self.color_True == (0, 0, 0):
+                        self.gameButton_True.setStyleSheet(f'background-color: rgb{self.color_True}; color: white;')
+                    else:
+                        self.gameButton_True.setStyleSheet(f'background-color: rgb{self.color_True}; color: black;')
+                    self.rgbInText(self.color_True, 'self.gameButton_True')
+
+    def set_colorr(self):
+        pass
 
     def FalseButton(self):
         if self.count - 5 > 0:
